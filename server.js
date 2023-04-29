71fd82a67dcf69661476a72f3b329a6a6f0c278b
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-04-30 01:15:23 +0300

    test

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const http = require('http')
 const fs = require('fs')
+const {start} = require('repl')
 
 const server = http.createServer((request, response) => {
-  switch (request.url) {
-    case '/home': {
-      const data = 'best free online course'
-      response.write(data)
-      response.end()
-      break
+    switch (request.url) {
+        case '/home': {
+            // 3 sec
+            const Start = new Date()
+            while (new Date() - start < 3000) {
+                console.log(new Date() - start)
+            }
+            const data = 'best free online course'
+            response.write(data)
+            response.end()
+            break
+        }
+        default: {
+            response.write('404 not found')
+            response.end()
+        }
+
     }
-    default: {
-      response.write('404 not found')
-      response.end()
-    }
-  }
 })
 
 server.listen(3003)
