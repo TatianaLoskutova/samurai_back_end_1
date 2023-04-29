2e561bfa4cd41bf2d791428091a59a101c400c72
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-04-30 02:57:36 +0300

    async added

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const http = require('http')
-const fs = require('fs')
-const {start} = require('repl')
+const fs = require('fs') // чтобы прога читала другой файл, подкулючаем модул fs = file system
 
-const server = http.createServer((request, response) => {
+const delay = (ms) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+} // проимистификация
+const readFile = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, (err, data) => {
+            if (err) reject(err)
+            else
+                resolve(data)
+        })  // (err -если все плохо будет ошибка, если ок, то data)
+    })
+
+}
+
+
+const server = http.createServer(async (request, response) => {
     switch (request.url) {
         case '/home': {
-            // 3 sec
-            const Start = new Date()
-            while (new Date() - start < 3000) {
-                console.log(new Date() - start)
+            try {
+                const data = await readFile('pages/about.html')
+                response.write(data)
+                response.end()
+            } catch (err) {
+                response.write('something wrong, 500')
+                response.end()
             }
-            const data = 'best free online course'
-            response.write(data)
+            break;
+        }  // (err -если все плохо будет ошибка, если ок, то data)
+        case '/about': {
+            await delay(3000)
+            response.write('About course')
             response.end()
-            break
+            break;
         }
         default: {
             response.write('404 not found')
             response.end()
         }
-
     }
 })
 
